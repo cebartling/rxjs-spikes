f9d3f2c00264b5d46f7f2b6c18f04edaede98237
Christopher Bartling <chris.bartling@gmail.com>
2016-01-03 09:25:55 -0600

    Adding in RxJS libraries.

--- rxjs-spike-001/karma.conf.js
+++ rxjs-spike-001/karma.conf.js
@@ -1,31 +1,32 @@
 'use strict';
 
-var path = require('path');
-var conf = require('./gulp/conf');
-var _ = require('lodash');
-var wiredep = require('wiredep');
+//var path = require('path');
+//var conf = require('./gulp/conf');
+//var _ = require('lodash');
+//var wiredep = require('wiredep');
+var webpack = require('karma-webpack');
 
-function listFiles() {
-    var wiredepOptions = _.extend({}, conf.wiredep, {
-        dependencies: true,
-        devDependencies: true
-    });
-
-    return wiredep(wiredepOptions).js
-        .concat([
-            path.join(conf.paths.tmp, '/serve/app/**/*.module.js'),
-            path.join(conf.paths.tmp, '/serve/app/**/*.js'),
-            path.join(conf.paths.tests, '/**/*.js'),
-            path.join(conf.paths.src, '/**/*.spec.js'),
-            path.join(conf.paths.src, '/**/*.mock.js'),
-            path.join(conf.paths.src, '/**/*.html')
-        ]);
-}
+//function listFiles() {
+//    var wiredepOptions = _.extend({}, conf.wiredep, {
+//        dependencies: true,
+//        devDependencies: true
+//    });
+//
+//    return wiredep(wiredepOptions).js
+//        .concat([
+//            path.join(conf.paths.tmp, '/serve/app/**/*.module.js'),
+//            path.join(conf.paths.tmp, '/serve/app/**/*.js'),
+//            path.join(conf.paths.tests, '/**/*.js'),
+//            path.join(conf.paths.src, '/**/*.spec.js'),
+//            path.join(conf.paths.src, '/**/*.mock.js'),
+//            path.join(conf.paths.src, '/**/*.html')
+//        ]);
+//}
 
 module.exports = function (config) {
 
     var configuration = {
-        files: listFiles(),
+        //files: listFiles(),
         singleRun: true,
         autoWatch: false,
         frameworks: ['jasmine'],
@@ -34,8 +35,40 @@ module.exports = function (config) {
             'karma-jasmine-html-reporter',
             'karma-chrome-launcher',
             'karma-phantomjs-launcher',
-            'karma-jasmine'
-        ]
+            'karma-jasmine',
+            'karma-webpack'
+        ],
+
+        files: [
+            // all files ending in "_test"
+            'test/*_test.js',
+            'test/**/*_test.js'
+            // each file acts as entry point for the webpack configuration
+        ],
+
+        preprocessors: {
+            // add webpack as preprocessor
+            'spec/*_spec.js': ['webpack'],
+            'spec/**/*_spec.js': ['webpack']
+        },
+
+        webpack: {
+            // karma watches the test entry points
+            // (you don't need to specify the entry option)
+            // webpack watches dependencies
+
+            // webpack configuration
+        },
+
+        webpackMiddleware: {
+            // webpack-dev-middleware configuration
+            // i. e.
+            noInfo: true
+        }
+
+        //plugins: [
+        //    require("karma-webpack")
+        //]
     };
 
     config.set(configuration);
